b8095600ecf09665023ef2e944ecb4c0546d0a4d
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-11-05 10:56:43 +0530

    Add Word Capitalization [A] from Codeforces

---
+++ 281/A - Word Capitalization/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>using namespace std; int main(){       string s;      cin>>s;       if(s[0]>=65&&s[0]<=90)      {            cout<<s;      }      else      {            s[0]=s[0]-32;            cout<<s;      }}
