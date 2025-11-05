03d6ea628ae221c02be2bf7034f63b87a7cfd80e
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-11-05 11:23:13 +0530

    Add Soldier and Bananas [A] from Codeforces

---
+++ 546/A - Soldier and Bananas/solution.cpp
@@ -0,0 +1,1 @@
+#include <iostream>using namespace std; int main(){      int k, n, w;      cin >> k >> n >> w;       int total = 0;       for (int i = 1; i <= w; i++)      {            total += i * k;      }      int borrow = total - n;       if (borrow < 0)      {            cout << 0;      }      else      {            cout << borrow;      }}
