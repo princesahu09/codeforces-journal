f25d44ab04f39cdc83214e1dc2d4f1fea84413cd
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-11-06 23:56:47 +0530

    Add Translation [A] from Codeforces

---
+++ 41/A - Translation/solution.cpp
@@ -0,0 +1,1 @@
+#include <iostream>#include <string>#include<algorithm>using namespace std; int main(){       string s;      cin >> s;      string t;      cin >> t;       reverse(t.begin(), t.end());      if (s == t)            cout << "YES" << endl;      else            cout << "NO" << endl;}
