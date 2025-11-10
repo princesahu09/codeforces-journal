819eebf8bc783fb880a54d51e20434587ffa4366
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2025-11-10 12:19:27 +0530

    Add Theatre Square [A] from Codeforces

---
+++ 1/A - Theatre Square/solution.java
@@ -0,0 +1,1 @@
+import java.util.*; public class Program1A{       public static void main(String[] args)      {             Scanner sc=new Scanner(System.in);             int n=sc.nextInt();            int m=sc.nextInt();            int a=sc.nextInt();              long ans=(long)Math.ceil((double)n/a)*(long)Math.ceil((double)m/a);            System.out.println(ans);      }}
