77762f81945f23a33f538eb91963fbcf140988ef
Prince sahu <169198696+princesahu09@users.noreply.github.com>
2026-02-01 21:35:44 +0530

    Add Grasshopper on a Line [A] from Codeforces

---
+++ 1837/A - Grasshopper on a Line/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>using namespace std; void solve(int x,int k){       if(x%k!=0)      {            cout<<1<<endl;            cout<<x<<endl;      }      else      {            cout<<2<<endl<<x-1<<" "<<1<<endl;      }}   int main(){        int t;      cin>>t;      while(t-- >0 )      {            int x,k;            cin>>x>>k;               solve(x,k);      }          return 0;}
